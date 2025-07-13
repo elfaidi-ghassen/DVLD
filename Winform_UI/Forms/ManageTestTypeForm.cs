a27db0de5cbdaca40642a4d0ca91ca43de584f72
elfaidi-ghassen <elfaidighassen@gmail.com>
2025-07-13 13:26:20 +0100

    VS project files updates

--- Winform_UI/Forms/ManageTestTypeForm.cs
+++ Winform_UI/Forms/ManageTestTypeForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DVLVBusinessLayer;
+using Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Util;
 
 namespace Winform_UI.Forms
 {
@@ -21,10 +24,44 @@ namespace Winform_UI.Forms
         {
             Close();
         }
+        private void InitColumns()
+        {
+            dgvTestTypes.Columns.Add("Column1", "ID");
+            dgvTestTypes.Columns.Add("Column2", "Title");
+            dgvTestTypes.Columns.Add("Column3", "Description");
+            dgvTestTypes.Columns.Add("Column3", "Fees");
 
+
+        }
+
+        private void LoadApplicationsTypes(List<TestType> types)
+        {
+            dgvTestTypes.Rows.Clear();
+
+            foreach (TestType type in types)
+            {
+                dgvTestTypes.Rows.Add(
+                        type.TestTypeID,
+                        type.TestTypeTitle,
+                        type.TestTypeDescription,
+                        type.TestTypeFees
+                    );
+            }
+        }
+        private void LoadApplicationTypes()
+        {
+            LoadApplicationsTypes(TestTypeManager.GetAllTestTypes());
+        }
+
+        private void UpdateCountLabel()
+        {
+            rowCountLabel.Text = $"# Records: {dgvTestTypes.RowCount}";
+        }
         private void ManageTestTypeForm_Load(object sender, EventArgs e)
         {
-
+            InitColumns();
+            LoadApplicationTypes();
+            UpdateCountLabel();
         }
     }
 }
