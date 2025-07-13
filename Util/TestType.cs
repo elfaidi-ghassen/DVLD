45c9e792fcec00d87d5706ffe67b2d08544c336d
elfaidi-ghassen <elfaidighassen@gmail.com>
2025-07-13 13:02:10 +0100

    Added TestType class that represents database table

---
+++ Util/TestType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public class TestType
+    {
+        public int TestTypeID { get; set; }
+        public string TestTypeTitle { get; set; }
+        public string TestTypeDescription { get; set; }
+        public decimal TestTypeFees { get; set; }
+
+        public TestType(int testTypeID, string testTypeTitle, string testTypeDescription, decimal testTypeFees)
+        {
+            TestTypeID = testTypeID;
+            TestTypeTitle = testTypeTitle;
+            TestTypeDescription = testTypeDescription;
+            TestTypeFees = testTypeFees;
+        }
+    }
+}
