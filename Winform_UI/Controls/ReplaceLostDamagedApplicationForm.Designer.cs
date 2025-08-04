c32868a812ccad91821bdd7eb749b2dde49112f3
elfaidi-ghassen <elfaidighassen@gmail.com>
2025-08-04 10:36:52 +0100

    added control to dispaly application details

---
+++ Winform_UI/Controls/ReplaceLostDamagedApplicationForm.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Winform_UI.Controls
+{
+    partial class ReplaceLostDamagedApplicationForm
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2GroupBox1 = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.labelAppId = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.labelOldLicenseID = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.labelCreatedBy = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.labelReplacedLicenseId = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelAppFees = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelAppDate = new System.Windows.Forms.Label();
+            this.guna2GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2GroupBox1
+            // 
+            this.guna2GroupBox1.Controls.Add(this.labelAppId);
+            this.guna2GroupBox1.Controls.Add(this.label);
+            this.guna2GroupBox1.Controls.Add(this.labelOldLicenseID);
+            this.guna2GroupBox1.Controls.Add(this.label5);
+            this.guna2GroupBox1.Controls.Add(this.label11);
+            this.guna2GroupBox1.Controls.Add(this.labelCreatedBy);
+            this.guna2GroupBox1.Controls.Add(this.label13);
+            this.guna2GroupBox1.Controls.Add(this.labelReplacedLicenseId);
+            this.guna2GroupBox1.Controls.Add(this.label1);
+            this.guna2GroupBox1.Controls.Add(this.labelAppFees);
+            this.guna2GroupBox1.Controls.Add(this.label3);
+            this.guna2GroupBox1.Controls.Add(this.labelAppDate);
+            this.guna2GroupBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2GroupBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(137)))), ((int)(((byte)(149)))));
+            this.guna2GroupBox1.Location = new System.Drawing.Point(3, 3);
+            this.guna2GroupBox1.Name = "guna2GroupBox1";
+            this.guna2GroupBox1.ShadowDecoration.Parent = this.guna2GroupBox1;
+            this.guna2GroupBox1.Size = new System.Drawing.Size(1028, 167);
+            this.guna2GroupBox1.TabIndex = 21;
+            this.guna2GroupBox1.Text = "Application";
+            // 
+            // labelAppId
+            // 
+            this.labelAppId.AutoSize = true;
+            this.labelAppId.BackColor = System.Drawing.Color.Transparent;
+            this.labelAppId.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelAppId.Location = new System.Drawing.Point(295, 49);
+            this.labelAppId.Name = "labelAppId";
+            this.labelAppId.Size = new System.Drawing.Size(42, 30);
+            this.labelAppId.TabIndex = 10;
+            this.labelAppId.Text = "[...]";
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.BackColor = System.Drawing.Color.Transparent;
+            this.label.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label.Location = new System.Drawing.Point(22, 49);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(100, 30);
+            this.label.TabIndex = 18;
+            this.label.Text = "App. ID:";
+            // 
+            // labelOldLicenseID
+            // 
+            this.labelOldLicenseID.AutoSize = true;
+            this.labelOldLicenseID.BackColor = System.Drawing.Color.Transparent;
+            this.labelOldLicenseID.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelOldLicenseID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelOldLicenseID.Location = new System.Drawing.Point(811, 82);
+            this.labelOldLicenseID.Name = "labelOldLicenseID";
+            this.labelOldLicenseID.Size = new System.Drawing.Size(42, 30);
+            this.labelOldLicenseID.TabIndex = 3;
+            this.labelOldLicenseID.Text = "[...]";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label5.Location = new System.Drawing.Point(538, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(247, 30);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Replaced License ID:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label11.Location = new System.Drawing.Point(538, 79);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(181, 30);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Old License ID:";
+            // 
+            // labelCreatedBy
+            // 
+            this.labelCreatedBy.AutoSize = true;
+            this.labelCreatedBy.BackColor = System.Drawing.Color.Transparent;
+            this.labelCreatedBy.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCreatedBy.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelCreatedBy.Location = new System.Drawing.Point(811, 112);
+            this.labelCreatedBy.Name = "labelCreatedBy";
+            this.labelCreatedBy.Size = new System.Drawing.Size(42, 30);
+            this.labelCreatedBy.TabIndex = 3;
+            this.labelCreatedBy.Text = "[...]";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label13.Location = new System.Drawing.Point(538, 112);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(144, 30);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Created By:";
+            // 
+            // labelReplacedLicenseId
+            // 
+            this.labelReplacedLicenseId.AutoSize = true;
+            this.labelReplacedLicenseId.BackColor = System.Drawing.Color.Transparent;
+            this.labelReplacedLicenseId.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelReplacedLicenseId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelReplacedLicenseId.Location = new System.Drawing.Point(811, 49);
+            this.labelReplacedLicenseId.Name = "labelReplacedLicenseId";
+            this.labelReplacedLicenseId.Size = new System.Drawing.Size(42, 30);
+            this.labelReplacedLicenseId.TabIndex = 5;
+            this.labelReplacedLicenseId.Text = "[...]";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(23, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 30);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Application Date:";
+            // 
+            // labelAppFees
+            // 
+            this.labelAppFees.AutoSize = true;
+            this.labelAppFees.BackColor = System.Drawing.Color.Transparent;
+            this.labelAppFees.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppFees.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelAppFees.Location = new System.Drawing.Point(295, 112);
+            this.labelAppFees.Name = "labelAppFees";
+            this.labelAppFees.Size = new System.Drawing.Size(42, 30);
+            this.labelAppFees.TabIndex = 7;
+            this.labelAppFees.Text = "[...]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Montserrat SemiBold", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label3.Location = new System.Drawing.Point(23, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Application Fees:";
+            // 
+            // labelAppDate
+            // 
+            this.labelAppDate.AutoSize = true;
+            this.labelAppDate.BackColor = System.Drawing.Color.Transparent;
+            this.labelAppDate.Font = new System.Drawing.Font("Montserrat", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAppDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.labelAppDate.Location = new System.Drawing.Point(295, 82);
+            this.labelAppDate.Name = "labelAppDate";
+            this.labelAppDate.Size = new System.Drawing.Size(42, 30);
+            this.labelAppDate.TabIndex = 9;
+            this.labelAppDate.Text = "[...]";
+            // 
+            // ReplaceLostDamagedApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.guna2GroupBox1);
+            this.Name = "ReplaceLostDamagedApplicationForm";
+            this.Size = new System.Drawing.Size(1036, 180);
+            this.guna2GroupBox1.ResumeLayout(false);
+            this.guna2GroupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GroupBox guna2GroupBox1;
+        private System.Windows.Forms.Label labelAppId;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label labelOldLicenseID;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label labelCreatedBy;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label labelReplacedLicenseId;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelAppFees;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelAppDate;
+    }
+}
